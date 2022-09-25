dee8c65be9bc79ab6b15be10f2a512a919ce1b5e
Juan Garcia <juanlds621@gmail.com>
2022-09-25 17:18:25 -0500

    counts

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     from sys import argv
     lenght = len(argv)
-    print("{:d} {:s}{:s}".format(lenght - 1, "argument" if lenght <= 2 else "arguments",
-                                "." if lenght == 1 else ":"))
+    print("{:d} {:s}{:s}".format(lenght - 1, "arguments"
+                                if lenght <= 2 else "arguments","." 
+                                if lenght == 1 else ":"))
     for i, s in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, s))
-            
