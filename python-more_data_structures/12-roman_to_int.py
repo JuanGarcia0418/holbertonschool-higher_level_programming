ebc125503b651ca565d6ed07f441bdc236520bcb
Juan Garcia <juanlds621@gmail.com>
2022-09-28 17:43:45 -0500

    numbers in roman

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    prev_val = 0
-    for i in range(len(roman_string)):
-        for key, val in num.items():
-            if key == roman_string[i]:
-                total += val
-                if prev_val < val:
-                    total -= (prev_val * 2)
-                    prev_val = val
-    return total
+    retval = 0
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    for i in roman_string:
+        if i in my_dict:
+            retval += my_dict[i]
+    for compare in range(0, len(roman_string) - 1):
+        if my_dict[roman_string[compare]] < my_dict[roman_string[compare + 1]]:
+            retval -= 2
+    return retval
