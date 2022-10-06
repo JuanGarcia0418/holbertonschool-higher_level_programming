a4a765e616053e82040acd980ac29be6d444fb21
Juan Garcia <juanlds621@gmail.com>
2022-10-06 09:57:14 -0500

    print name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"Module with function that prints My name is <first name> <last name>"
+
+
+def say_my_name(first_name, last_name=""):
+    "Function that prints My name is <first name> <last name>"
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    if first_name == "" and last_name == "":
+        print("My name is")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
