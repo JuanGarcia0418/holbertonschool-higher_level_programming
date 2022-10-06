632c6f0603a543c7eea8149d21fb9cee3f9c37b4
Juan Garcia <juanlds621@gmail.com>
2022-10-06 00:45:28 -0500

    print size of the square

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """square private instance attribute size"""
+    def __init__(self, size):
+        """instantiation with size"""
+        self.__size = size
