76194fb9a90a3d9ae4811440c185317b6dc363ef
Juan Garcia <juanlds621@gmail.com>
2022-10-06 01:25:32 -0500

    print the character with the size is 0

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -31,6 +31,6 @@ class Square:
         if self.__size == 0:
             print("")
         for i in range(self.__size):
-            for j in range (self.__size):
+            for j in range(self.__size):
                 print('#', end="")
             print("")
