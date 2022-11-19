080ab2c1aaa4a9ea0f042e638e2c67b0d6428eed
Juan Garcia <juanlds621@gmail.com>
2022-11-18 21:19:13 -0500

    add SQL injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 5:
         db = MySQLdb.connect(host="localhost", port=3306,
-                            user=sys.argv[1],
-                            passwd=sys.argv[2],
-                            db=sys.argv[3])
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
 
         cursor = db.cursor()
         st = sys.argv[4].split('\'')
