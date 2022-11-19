e29c720d01121ae90dc466f7b64aea21fefc61e7
Juan Garcia <juanlds621@gmail.com>
2022-11-18 22:16:09 -0500

    add class of model of state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""contains the class definition of a State and an instance Base = declarative_base():"""
+"""contains the class definition of a State and an instance"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     """Class for state"""
