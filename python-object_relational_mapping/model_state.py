2b320e77a1ddee4d8880c206271c339ce5bcf2ce
Juan Garcia <juanlds621@gmail.com>
2022-11-18 22:17:00 -0500

    add class of model of state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,6 @@ Base = declarative_base()
 class State(Base):
     """Class for state"""
     __tablename__ = 'states'
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
