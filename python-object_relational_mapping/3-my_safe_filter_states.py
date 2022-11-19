293b43324b66f7a368929d99db315cfb0e0d48c0
Juan Garcia <juanlds621@gmail.com>
2022-11-18 21:00:11 -0500

    add SQL injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python3
-"""Display name argument of states table"""
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 
-def filter_names_safe():
-    
+if __name__ == "__main__":
+
     if len(sys.argv) == 5:
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3])
+        db = MySQLdb.connect(host="localhost", port=3306,
+                            user=sys.argv[1],
+                            passwd=sys.argv[2],
+                            db=sys.argv[3])
 
         cursor = db.cursor()
+        cursor.execute("""
+                    SELECT *
+                    FROM states
+                    WHERE BINARY name='{:s}'
+                    ORDER BY id ASC
+                    """.format(sys.argv[4]))
 
-        cursor.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
-                    ORDER BY id ASC".format(sys.argv[4]))
         rows = cursor.fetchall()
-        for i in rows:
-            print(i)
+        for row in rows:
+            if row[1] == sys.argv[4]:
+                print(row)
 
         cursor.close()
         db.close()
-    else:
-        return
-
-if __name__ == "__main__":
-    filter_names_safe()
