89e44330256b6ee6c5d8edff8e0ada347b83430e
Juan Garcia <juanlds621@gmail.com>
2022-11-18 22:57:38 -0500

    search state in database

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    rows = session.query(State).all()
+    res = ""
+
+    for row in rows:
+        if sys.argv[4] in row.name:
+            res = row.id            
+    if res != "":
+        print(res)
+    else:
+        print("Not Found")
+        
+    session.close()
