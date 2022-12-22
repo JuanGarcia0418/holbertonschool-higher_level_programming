9c99263ba6898cb0015d9639d42b0349734856fc
Juan Garcia <juanlds621@gmail.com>
2022-12-22 19:41:10 +0000

    add starwarstitle

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/';
+request(url + process.argv[2], function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const name = JSON.parse(body);
+    console.log(name.title);
+  } else {
+    console.log(error);
+  }
+});
