e890d1d316148a02358447d527a48ab7e1cb6ab3
Juan Garcia <juanlds621@gmail.com>
2022-12-22 19:51:15 +0000

    request content of a webpage

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(process.argv[3], body, 'utf-8');
+  } else {
+    console.log(error);
+  }
+});
