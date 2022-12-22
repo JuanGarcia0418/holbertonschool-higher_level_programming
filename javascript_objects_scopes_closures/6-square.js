c79734fa2a21739bf2848cfb5b38d3990a0c40d8
Juan Garcia <juanlds621@gmail.com>
2022-12-22 18:06:21 +0000

    change character of square

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Oldsquare = require('./5-square.js');
+
+module.exports = class Square extends Oldsquare {
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let index = 0; index < this.height; index++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+};
